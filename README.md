a5cb6baf7909c43cea1049b4add265955d3d514d
khadijashahzad565 <khadijashahzad565@gmail.com>
2025-07-29 22:39:25 +0500

    new changes

---
+++ README.md
@@ -0,0 +1,1 @@
+# Naat Research Center
---
+++ wp-config.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the website, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'naat-research-center' );
+
+/** Database username */
+define( 'DB_USER', 'root' );
+
+/** Database password */
+define( 'DB_PASSWORD', '' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+
+
+// DigitalOcean Spaces credentials for WordPress S3 plugin
+// if ( $_SERVER['HTTP_HOST'] === 'localhost' ) {
+//     define( 'AS3CF_SETTINGS', serialize( array(
+//         'provider'         => 'do',
+//         'access-key-id'    => 'DO801DNRR64YWJYFVA9W',
+//         'secret-access-key'=> 'mlGIxJ/JoPbKLHGT7lLUcGBOWHHME7BGsZtYg0XYoUw',
+//         'cloudfront' => 'https://nrc-books.blr1.cdn.digitaloceanspaces.com'
+//     ) ) );
+// } else {
+//     define( 'AS3CF_SETTINGS', serialize( array(
+//         'provider'         => 'do',
+//         'access-key-id'    => 'DO801DNRR64YWJYFVA9W',
+//         'secret-access-key'=> 'mlGIxJ/JoPbKLHGT7lLUcGBOWHHME7BGsZtYg0XYoUw',
+//     ) ) );
+// }
+define( 'WP_MEMORY_LIMIT', '256M' );
+
+// DigitalOcean Spaces credentials for WordPress S3 plugin
+if ( $_SERVER['HTTP_HOST'] === 'localhost' ) {
+    define( 'AS3CF_SETTINGS', serialize( array(
+        'provider'         => 'do',
+        'access-key-id'    => 'DO801DNRR64YWJYFVA9W',
+        'secret-access-key'=> 'mlGIxJ/JoPbKLHGT7lLUcGBOWHHME7BGsZtYg0XYoUw',
+    ) ) );
+} else {
+    define( 'AS3CF_SETTINGS', serialize( array(
+        'provider'         => 'do',
+        'access-key-id'    => 'DO801DNRR64YWJYFVA9W',
+        'secret-access-key'=> 'mlGIxJ/JoPbKLHGT7lLUcGBOWHHME7BGsZtYg0XYoUw',
+    ) ) );
+}
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    define('WP_HOME', 'http://localhost/naat-research-center');
+    define('WP_SITEURL', 'http://localhost/naat-research-center');
+} else {
+    define('WP_HOME', 'https://naatresearchcenter.org');
+    define('WP_SITEURL', 'https://naatresearchcenter.org');
+}
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
